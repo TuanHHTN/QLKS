fde1bd02cf56b78130fbf759728631e38420dd0f
sinhvien <sinhvien>
2018-01-06 15:43:03 +0700

    thong tin

---
+++ QuanLyKhachSan/QuanLyKhachSan/FormThongTin.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace QuanLyKhachSan
+{
+    partial class FormThongTin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbllogo = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbllogo
+            // 
+            this.lbllogo.AutoSize = true;
+            this.lbllogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.lbllogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbllogo.Location = new System.Drawing.Point(168, 27);
+            this.lbllogo.Name = "lbllogo";
+            this.lbllogo.Size = new System.Drawing.Size(207, 29);
+            this.lbllogo.TabIndex = 19;
+            this.lbllogo.Text = "Quản lý khách sạn";
+            // 
+            // FormThongTin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(547, 338);
+            this.Controls.Add(this.lbllogo);
+            this.Name = "FormThongTin";
+            this.Text = "FormThongTin";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label lbllogo;
+    }
+}
