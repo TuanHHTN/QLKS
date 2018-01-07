dce8250b47b74b6e3cbb0cdd8bebf284013e55ce
PhamTuan <phamtuan280497@mgmail.com>
2018-01-07 21:43:14 +0700

    dieu chinh vi tri giao dien

--- QuanLyKhachSan/QuanLyKhachSan/FormPhong.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/FormPhong.Designer.cs
@@ -306,7 +306,7 @@
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(712, 460);
+            this.ClientSize = new System.Drawing.Size(672, 432);
             this.Controls.Add(this.tabControl1);
             this.Name = "FormPhong";
             this.Text = "FormPhong";
