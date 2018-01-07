bf20d1bbbd9a056e304f989debb4f75e687d9572
PhamTuan <phamtuan280497@mgmail.com>
2018-01-07 21:48:51 +0700

    them thong tin

--- QuanLyKhachSan/QuanLyKhachSan/FormThongTin.Designer.cs
+++ QuanLyKhachSan/QuanLyKhachSan/FormThongTin.Designer.cs
@@ -29,24 +29,38 @@
         private void InitializeComponent()
         {
             this.lbllogo = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // lbllogo
             // 
             this.lbllogo.AutoSize = true;
             this.lbllogo.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
-            this.lbllogo.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(192)))));
+            this.lbllogo.ForeColor = System.Drawing.Color.Black;
             this.lbllogo.Location = new System.Drawing.Point(168, 27);
             this.lbllogo.Name = "lbllogo";
             this.lbllogo.Size = new System.Drawing.Size(207, 29);
             this.lbllogo.TabIndex = 19;
             this.lbllogo.Text = "Quản lý khách sạn";
             // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(163)));
+            this.label1.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.label1.Location = new System.Drawing.Point(151, 132);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(254, 75);
+            this.label1.TabIndex = 20;
+            this.label1.Text = "Developer:      Pham Tuan\r\n\r\n                       Vo Duy Thanh";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
             // FormThongTin
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(547, 338);
+            this.Controls.Add(this.label1);
             this.Controls.Add(this.lbllogo);
             this.Name = "FormThongTin";
             this.Text = "FormThongTin";
@@ -57,5 +71,6 @@
 
         #endregion
         private System.Windows.Forms.Label lbllogo;
+        private System.Windows.Forms.Label label1;
     }
 }
